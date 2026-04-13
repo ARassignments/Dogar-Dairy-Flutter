e0bd152bf865721893d98f8cb66fb00dc57cf643
AR Assignments <abdurrehman905623@gmail.com>
2026-04-13 23:32:22 +0500

    uploads

---
+++ README.md
@@ -0,0 +1,3 @@
+# dogardairy
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dogardairy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dogardairy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
